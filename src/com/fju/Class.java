647703b7fa4a7d900d24dcf38ed77e4a9e0a082b
ju <a0920652633@gmail.com>
2021-03-09 15:52:38 +0800

    initial commit

---
+++ src/com/fju/Class.java
@@ -0,0 +1,17 @@
+package com.fju;
+
+public class Class {
+    String name;
+    int english;
+    int math;
+
+    public Class(String name, int english, int math){
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+    public int average(){
+        return (english + )
+    }
+
+}
---
+++ src/com/fju/Person.java
@@ -0,0 +1,27 @@
+package com.fju;
+
+public class Person {
+    private float weight;
+    private float height;
+
+    public Person(float weight, float height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
