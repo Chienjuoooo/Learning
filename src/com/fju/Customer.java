bb6f6f5a1e04f34486331f24dc9af44903712cbc
ju <a0920652633@gmail.com>
2021-03-13 17:45:30 +0800

    initial commit

---
+++ src/com/fju/Customer.java
@@ -0,0 +1,17 @@
+package com.fju;
+
+public class Customer {
+    String id;
+    int money;
+    int discount;
+    public Customer(String id, int money, int discount){
+        this.id = id;
+        this.money = money;
+        this.discount = discount;
+    }
+    public void print() {
+        System.out.println(id + "\t" + money + "\t" + discount + "\t" +(money-discount));
+
+
+    }
+}
