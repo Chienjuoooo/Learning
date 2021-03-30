392061ca116c966a5e2339174c4e818d3c8aaefe
ju <a0920652633@gmail.com>
2021-03-30 16:38:49 +0800

    box commit

---
+++ src/com/fju/Box3.java
@@ -0,0 +1,18 @@
+package com.fju;
+
+public class Box3 extends Box{
+    public Box3() {
+        int length = 10;
+        int width = 10;
+        int height = 10;
+    }
+    @Override
+    public String getName() {
+        return "BOX 3";
+    }
+
+    @Override
+    public int getPrice() {
+        return 60;
+    }
+}
